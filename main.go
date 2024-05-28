b7bf8f1a006586a4dc3ca1277b9c70212dc744ce
danilo-sgalvao <danilo.galvao@discente.ufg.br>
2024-05-28 15:27:38 -0300

    add 2 pag + css

--- main.go
+++ main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"database/sql"
+	//"database/sql"
 	"encoding/json"
-	"fmt"
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+	//"fmt"
+	//"github.com/joho/godotenv"
 	"io"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
-var db = fazConexaoComBanco()
+//var db = fazConexaoComBanco()
 
 func main() {
 	// Configuração do servidor para servir arquivos estáticos (HTML, CSS, JS, imagens, etc.)
 	fs := http.FileServer(http.Dir("./"))
 	http.Handle("/", fs)
 
-	alimentaBancoDeDados()
+	//alimentaBancoDeDados()
 
 	log.Println("Server rodando na porta 8080")
 	// Inicia o servidor na porta 8080
@@ -29,39 +30,39 @@ func main() {
 	}
 }
 
-func fazConexaoComBanco() *sql.DB {
-	// carrega arquivo .env com dados de conexão com o banco
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Erro ao carregar arquivo .env")
-	}
+//func fazConexaoComBanco() *sql.DB {
+// carrega arquivo .env com dados de conexão com o banco
+//	err := godotenv.Load()
+//	if err != nil {
+//		log.Fatalf("Erro ao carregar arquivo .env")
+//	}
 
-	// faz a busca dos atributos no arquivo .env para usa-las na conexão com banco
-	usuarioBancoDeDados := os.Getenv("USUARIO")
-	senhaDoUsuario := os.Getenv("SENHA")
-	nomeDoBancoDeDados := os.Getenv("NOME_BANCO_DE_DADOS")
-	dadosParaConexao := "user=" + usuarioBancoDeDados + " dbname=" + nomeDoBancoDeDados + " password=" + senhaDoUsuario + " host=localhost port=5432 sslmode=disable"
-	database, err := sql.Open("postgres", dadosParaConexao)
-	if err != nil {
-		fmt.Println(err)
-	}
+// faz a busca dos atributos no arquivo .env para usa-las na conexão com banco
+//	usuarioBancoDeDados := os.Getenv("USUARIO")
+///	senhaDoUsuario := os.Getenv("SENHA")
+//	nomeDoBancoDeDados := os.Getenv("NOME_BANCO_DE_DADOS")
+//	dadosParaConexao := "user=" + usuarioBancoDeDados + " dbname=" + nomeDoBancoDeDados + " password=" + senhaDoUsuario + " host=localhost port=5432 sslmode=disable"
+//	database, err := sql.Open("postgres", dadosParaConexao)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
 
-	// cria tabela paciente com atributos como: id, nome, cpf, data de nascimento, telefone, sexo e booleanos referente a situação fisica
-	_, err = database.Query("CREATE TABLE IF NOT EXISTS paciente (id SERIAL PRIMARY KEY, nome VARCHAR(255) UNIQUE NOT NULL, cpf VARCHAR(15) UNIQUE NOT NULL, data_nascimento VARCHAR(12), telefone_celular VARCHAR(20), sexo VARCHAR(10), esta_fumante boolean, faz_uso_alcool boolean, esta_situacao_rua boolean)")
-	if err != nil {
-		log.Fatal(err)
-	}
+// cria tabela paciente com atributos como: id, nome, cpf, data de nascimento, telefone, sexo e booleanos referente a situação fisica
+//	_, err = database.Query("CREATE TABLE IF NOT EXISTS paciente (id SERIAL PRIMARY KEY, nome VARCHAR(255) UNIQUE NOT NULL, cpf VARCHAR(15) UNIQUE NOT NULL, data_nascimento VARCHAR(12), telefone_celular VARCHAR(20), sexo VARCHAR(10), esta_fumante boolean, faz_uso_alcool boolean, esta_situacao_rua boolean)")
+//	if err != nil {
+//		log.Fatal(err)
+//	}
 
-	return database
-}
+//	return database
+//}
 
-func cadastraPaciente(paciente Paciente) {
-	// insere paciente no banco de dados
-	_, err := db.Exec(`INSERT INTO paciente (nome, cpf, data_nascimento, telefone_celular, sexo, esta_fumante, faz_uso_alcool, esta_situacao_rua) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) on conflict do nothing`, paciente.Nome, paciente.Cpf, paciente.DataNascimento, paciente.Telefone, paciente.Sexo, paciente.EstaFumante, paciente.FazUsoAlcool, paciente.EstaSituacaoDeRua)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
+//func cadastraPaciente(paciente Paciente) {
+// insere paciente no banco de dados
+//_, err := db.Exec(`INSERT INTO paciente (nome, cpf, data_nascimento, telefone_celular, sexo, esta_fumante, faz_uso_alcool, esta_situacao_rua) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) on conflict do nothing`, paciente.Nome, paciente.Cpf, paciente.DataNascimento, paciente.Telefone, paciente.Sexo, paciente.EstaFumante, paciente.FazUsoAlcool, paciente.EstaSituacaoDeRua)
+//if err != nil {
+//		fmt.Println(err)
+//	}
+//}
 
 func alimentaBancoDeDados() {
 	var Pacientes Pacientes
@@ -76,7 +77,7 @@ func alimentaBancoDeDados() {
 	}
 
 	for i := 0; i < len(Pacientes.Pacientes); i++ {
-		cadastraPaciente(Pacientes.Pacientes[i])
+		//cadastraPaciente(Pacientes.Pacientes[i])
 	}
 }
 
